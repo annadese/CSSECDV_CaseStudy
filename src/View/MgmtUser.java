1238a3ab5e5900a14225a0399b12d5aab18fc03b
anna_dese <anna_desembrana@dlsu.edu.ph>
2023-04-01 02:21:08 +0800

    Implemented User - Delete for ADMIN

--- src/View/MgmtUser.java
+++ src/View/MgmtUser.java
@@ -201,6 +201,7 @@ public class MgmtUser extends javax.swing.JPanel {
             
             if (result == JOptionPane.YES_OPTION) {
                 System.out.println(tableModel.getValueAt(table.getSelectedRow(), 0));
+                sqlite.removeUser((String)tableModel.getValueAt(table.getSelectedRow(), 0));
             }
         }
     }//GEN-LAST:event_deleteBtnActionPerformed
